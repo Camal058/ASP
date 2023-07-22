daafb5ea0319758face60adf67fceaae09cd16e4
Camal <87723982+Camal058@users.noreply.github.com>
2023-07-22 18:18:52 +0400

    first

---
+++ LoginRegistration/LoginRegistration/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace LoginRegistration.Models;
+
+public class User
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+}
---
+++ LoginRegistration/LoginRegistration/Pages/Login.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoginRegistration.Pages;
+
+public class LoginModel : PageModel
+{
+    [BindProperty]
+    public string Username { get; set; }
+
+    [BindProperty]
+    public string Password { get; set; }
+
+    public void OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            bool isAuthenticated = UserRepository.AuthenticateUser(Username, Password);
+            if (isAuthenticated)
+            {
+                // Redirect to the desired page upon successful login
+                Response.Redirect("/");
+            }
+            else
+            {
+                // Display an error message
+                ModelState.AddModelError(string.Empty, "Invalid username or password");
+            }
+        }
+    }
+}
