7a29d7068403c3c1d52909f925355b232bd421e2
Camal <87723982+Camal058@users.noreply.github.com>
2023-09-08 12:02:33 +0400

    HW3

---
+++ CurrencyConverter/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using CurrencyConverter.Helpers;
+using CurrencyConverter.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CurrencyConverter.Pages;
+
+public class IndexModel : PageModel
+{
+    public CurrencyConversionModel CurrencyConversionModel { get; set; }
+
+    public void OnGet()
+    {
+        CurrencyConversionModel = new CurrencyConversionModel
+        {
+            Currencies = XmlDocumentHelper.FetchDataFromXml()
+        };
+    }
+}
+
